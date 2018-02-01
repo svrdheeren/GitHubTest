f6229cd26ae84c9f0da4ef16fd6f82b9a36d7006
Dheerendra Sharma <dheeren.sharma@gmail.com>
2018-01-31 19:04:05 -0600

    Commit Project

---
+++ GitHubTest/src/testGit/testingGit.java
@@ -0,0 +1,16 @@
+package testGit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class testingGit {
+
+	public static void main(String[] args){
+		System.setProperty("webdriver.chrome.driver", "E:\\Selenium Tutorial\\chromedriver_win32\\chromedriver.exe");
+		WebDriver WD = new ChromeDriver();
+		WD.get("http://facebook.com");
+		WD.findElement(By.id("email")).sendKeys("Test");
+		WD.quit();
+	}
+}
